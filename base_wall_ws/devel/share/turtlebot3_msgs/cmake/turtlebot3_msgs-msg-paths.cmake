df30cd9f0207f1e1d8b7efa38e17a03cb3c64c5c
joshbindels <josh.bindels@gmail.com>
2020-06-18 17:08:36 +0200

    Remove devel and build from git

--- base_wall_ws/devel/share/turtlebot3_msgs/cmake/turtlebot3_msgs-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(turtlebot3_msgs_MSG_INCLUDE_DIRS "/home/ros/p4_base_wall/ar_base_wall/base_wall_ws/src/turtlebot3_msgs/msg")
-set(turtlebot3_msgs_MSG_DEPENDENCIES std_msgs)
